84acfbe4b201dbbbbd7704c0cf12d97344f5a14b
KonstantinAks <silverzilberman@gmail.com>
2022-07-23 16:33:05 +0300

    homework 1

---
+++ cypress/selectors/sign_in_page.js
@@ -0,0 +1,17 @@
+export const sign_in_page = {
+    logo_image: '.makeStyles-logo-3',
+    title_text: '.MuiTypography-h5',
+    fieldUsername: '#username',
+    fieldPassword: '#password',
+    placeholderPassword: '#password-label',
+    placeholderUsername: '#username-label',
+    outside: '.makeStyles-root-1',
+    checkboxOn: '.PrivateSwitchBase-input-14',
+    checkboxOff: '.PrivateSwitchBase-input-14',
+    sinnInDisabled: '[data-test="signin-submit"]',
+    have_Dont_have_an_account_Sign_Up: '[data-test="signup"]',
+    haveText: '[data-test="signup"]',
+    click_Link: '[data-test="signup"]',
+    licenseText: '.MuiTypography-body2',
+    copyright: '[rel="noopener noreferrer"]'
+}
