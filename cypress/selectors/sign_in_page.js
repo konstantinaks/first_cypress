cf4d362984fd7f823342ac6934948c200ba58aab
KonstantinAks <silverzilberman@gmail.com>
2022-07-24 23:35:07 +0300

    homework2

--- cypress/selectors/sign_in_page.js
+++ cypress/selectors/sign_in_page.js
@@ -13,5 +13,24 @@ export const sign_in_page = {
     haveText: '[data-test="signup"]',
     click_Link: '[data-test="signup"]',
     licenseText: '.MuiTypography-body2',
-    copyright: '[rel="noopener noreferrer"]'
+    copyright: '[rel="noopener noreferrer"]',
+    first_Name: '#firstName',
+    last_Name: '#lastName',
+    username: '#username',
+    password: '#password',
+    ConfirmPassword: '#confirmPassword',
+    Signup_Submit: '[data-test="signup-submit"]',
+    sign_in: '[data-test="signin-submit"]',
+    modal: '[data-test="user-onboarding-dialog-content"]',
+    clickNext: '[data-test="user-onboarding-next"]',
+    bankName: '#bankaccount-bankName-input',
+    routing_number: '#bankaccount-routingNumber-input',
+    account_number: '#bankaccount-accountNumber-input',
+    click_save_bank: '[data-test="bankaccount-submit"]',
+    click_done: '[data-test="user-onboarding-next"]',
+    logout_btn: '[data-test="sidenav-signout"]'
+
+
+
+
 }
